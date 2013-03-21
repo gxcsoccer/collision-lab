a23ec61de6b74cc52f967a58420c9c5ff80af5ce
gaoxiaochen <gxcsoccer@126.com>
2013-03-22 02:15:05 +0800

    add AABB collision detection

---
+++ js/AABB.js
@@ -0,0 +1,95 @@
+window.requestAnimFrame = this.requestAnimFrame = (function() {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+	function(callback, element) {
+		window.setTimeout(callback, 1000 / 60);
+	};
+})();
+
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		rect1 = new Rect(new Vector2d(80, 150), 100, 100),
+		rect2 = new Rect(new Vector2d(250, 70), 50, 50),
+		objs = [rect1, rect2],
+		dragObj, dragOffset;
+
+	function getPoint(e) {
+		var x, y;
+
+		if (e.layerX != null) {
+			x = e.layerX;
+			y = e.layerY;
+		} else {
+			x = e.offsetX;
+			y = e.offsetY;
+		}
+
+		return {
+			x: x,
+			y: y
+		}
+	};
+
+	function distance(p1, p2) {
+		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y))
+	};
+
+	canvas.addEventListener("mousedown", function(e) {
+		var mp = getPoint(e);
+		objs.forEach(function(o) {
+			if (distance(o.center, mp) <= 8) {
+				dragObj = o;
+				dragOffset = {
+					x: mp.x - o.center.x,
+					y: mp.y - o.center.y
+				}
+				return false;
+			}
+		})
+
+	}, false);
+	canvas.addEventListener("mousemove", function(e) {
+		var mp = getPoint(e);
+		if (dragObj) {
+			dragObj.center.x = mp.x - dragOffset.x;
+			dragObj.center.y = mp.y - dragOffset.y;
+		}
+	}, false);
+	canvas.addEventListener("mouseup", function(e) {
+		dragObj && (dragObj = null);
+	}, false);
+
+
+	function checkCollision(rect1, rect2) {
+		if ((rect2.center.x + rect2.width / 2 < rect1.center.x - rect1.width / 2) || 
+			(rect1.center.x + rect1.width / 2 < rect2.center.x -rect2.width / 2) || 
+			(rect2.center.y + rect2.height / 2 < rect1.center.y - rect1.height / 2) || 
+			(rect1.center.y + rect1.height / 2 < rect2.center.y - rect2.height / 2)) {
+			return false;
+		} else {
+			return true;
+		}
+	};
+
+	context.font = "20px Calibri";
+
+	function draw(context) {
+		context.clearRect(0, 0, 400, 300);
+
+		objs.forEach(function(o) {
+			o.draw(context);
+		});
+
+		if (checkCollision(rect1, rect2)) {
+			context.fillText("Collision", 10, 20);
+		} else {
+			context.fillText("Separated", 10, 20);
+		}
+
+		requestAnimFrame(function() {
+			draw(context);
+		});
+	}
+
+	draw(context);
+};
---
+++ js/Rect.js
@@ -0,0 +1,46 @@
+var Rect = function(center, width, height, deg) {
+		this.center = center;
+		this.width = width;
+		this.height = height;
+		this.deg = deg || 0;
+	}
+
+Rect.prototype = {
+	draw: function(context) {
+		var points = this.getPoints(),
+			prePoint;
+
+		context.beginPath();
+		points.forEach(function(p) {
+			prePoint && context.lineTo(p.x, p.y);
+			context.moveTo(p.x, p.y);
+			prePoint = p;
+		});
+		prePoint && context.lineTo(points[0].x, points[0].y);
+		context.stroke();
+		context.closePath();
+		context.arc(this.center.x, this.center.y, 8, 0, Math.PI * 2, false);
+		context.fill();
+	},
+	getPoints: function() {
+		var points = [],
+			uv1 = Vector2d.unitVector(this.deg),
+			uv2 = Vector2d.unitVector(this.deg + 90),
+			uv3 = Vector2d.unitVector(-this.deg),
+			uv4 = Vector2d.unitVector(90 - this.deg),
+			cx = this.center.dotProduct(uv1),
+			cy = this.center.dotProduct(uv2);
+
+		points.push(new Vector2d(cx - this.width / 2, cy - this.height / 2));
+		points.push(new Vector2d(cx + this.width / 2, cy - this.height / 2));
+		points.push(new Vector2d(cx + this.width / 2, cy + this.height / 2));
+		points.push(new Vector2d(cx - this.width / 2, cy + this.height / 2));
+
+		return points.map(function(p) {
+			return new Vector2d(p.dotProduct(uv3), p.dotProduct(uv4));
+		});
+	},
+	getVectors: function() {
+
+	}
+}
